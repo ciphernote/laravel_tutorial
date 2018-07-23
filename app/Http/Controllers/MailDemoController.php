4482ff371fc969c62ff527af892214e028b9ad8e
y.shimo <evedon4080@gmail.com>
2018-07-24 00:21:59 +0900

    apps

--- app/Http/Controllers/MailDemoController.php
+++ app/Http/Controllers/MailDemoController.php
@@ -24,7 +24,7 @@ class MailDemoController extends Controller
     public function finish(\App\Http\Requests\MailDemoRequest $request)
     {
         $data = $request->all();
-        $subject = "【自動配信メール】お問い合わせありがとうございます"
+        $subject = "【自動配信メール】お問い合わせありがとうございます";
         Mail::send('mail.temp', $data, function($message) use($data){
             $message->to($data["email"])->subject($subject);
         });
