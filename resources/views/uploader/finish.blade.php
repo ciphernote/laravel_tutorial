ecc554b0ff512a0744ece7e2d88b318ff7cf221f
y.shimo <evedon4080@gmail.com>
2018-07-24 11:01:28 +0900

    apps

--- resources/views/uploader/finish.blade.php
+++ resources/views/uploader/finish.blade.php
@@ -6,7 +6,7 @@
 <p>入力画面 -> 確認画面 -> <span class="label label-danger">完了画面</span></p>
 
 <div class="alert alert-success" role="alert">データベースに画像を登録しました！</div>
-<div class="col-sm-offset-4 col-sm-8 text-center">
+<div class="col-sm-offset-2 col-sm-10 text-center">
     <a href="{{ route('uploader.index')}}" class="btn btn-embossed btn-primary btn-wide">TOPへ</a>
 </div>
 @endsection
